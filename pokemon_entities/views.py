f15a5b85a8b23fa78c96e3ca8b3874dc387fa6d6
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-04-23 19:52:23 +0300

    Disappearing of pokemons
    Of disappeared and not appeared

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.utils.timezone import localtime
 from .models import Pokemon, PokemonEntity
 
 
@@ -28,19 +29,18 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
+    pokemons = Pokemon.objects.all()
+    now = localtime()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
+        for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon, appeared_at__lt=now, disappeared_at__gt=now):
             add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
+                folium_map, pokemon_entity.latitude,
+                pokemon_entity.longitude,
+                request.build_absolute_uri(pokemon.image.url)
             )
 
-    pokemons = Pokemon.objects.all()
+    
     pokemons_on_page = []
     for pokemon in pokemons:
         pokemons_on_page.append({
