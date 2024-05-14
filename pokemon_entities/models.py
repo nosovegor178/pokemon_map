09078f015d0082a2d95f7dff47ba08affb1f382f
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-05-14 18:27:39 +0300

    Добавил для всех полей моделей verbose_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,29 +5,59 @@ import datetime  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(blank=True, null=True)
-    description = models.CharField(max_length=1000, blank=True, null=True)
-    evolution = models.ForeignKey("self", on_delete=models.CASCADE,
-                                           blank=True, null=True, related_name='next')
+    title = models.CharField(max_length=200,
+                             verbose_name='Название покемона')
+    title_en = models.CharField(max_length=200,
+                                blank=True,
+                                verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, 
+                                blank=True,
+                                verbose_name='Название на японском')
+    image = models.ImageField(blank=True,
+                              null=True,
+                              verbose_name='Изображение покемона')
+    description = models.CharField(max_length=1000,
+                                   blank=True,
+                                   null=True,
+                                   verbose_name='Описание')
+    evolution = models.ForeignKey("self",
+                                  on_delete=models.CASCADE,
+                                  blank=True,
+                                  null=True,
+                                  related_name='next',
+                                  verbose_name='Эволюции покемона')
 
     def __str__(self):
         return f'{self.title}'
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE,
+                                verbose_name='Ссылка на покемона')
     
-    latitude = models.FloatField(max_length=200)
-    longitude = models.FloatField(max_length=200)
+    latitude = models.FloatField(max_length=200,
+                                 verbose_name='Широта дислокации покемона')
+    longitude = models.FloatField(max_length=200,
+                                  verbose_name='Долгота дислокации покемона')
 
-    appeared_at = models.DateTimeField(default=datetime.datetime.now())
-    disappeared_at = models.DateTimeField(default=datetime.datetime.now())
+    appeared_at = models.DateTimeField(default=datetime.datetime.now(),
+                                       verbose_name='Дата и время появления')
+    disappeared_at = models.DateTimeField(default=datetime.datetime.now(),
+                                          verbose_name='Дата и время исчезновения')
 
-    Level = models.IntegerField(db_index=True, null=True)
-    Health = models.IntegerField(db_index=True, null=True)
-    Strength = models.IntegerField(db_index=True, null=True)
-    Defence = models.IntegerField(db_index=True, null=True)
-    Stamina = models.IntegerField(db_index=True, null=True)
+    Level = models.IntegerField(db_index=True,
+                                null=True,
+                                verbose_name='Уровень')
+    Health = models.IntegerField(db_index=True,
+                                 null=True,
+                                 verbose_name='Здоровье')
+    Strength = models.IntegerField(db_index=True,
+                                   null=True,
+                                   verbose_name='Сила')
+    Defence = models.IntegerField(db_index=True,
+                                  null=True,
+                                  verbose_name='Защита')
+    Stamina = models.IntegerField(db_index=True,
+                                  null=True,
+                                  verbose_name='Выносливость')
